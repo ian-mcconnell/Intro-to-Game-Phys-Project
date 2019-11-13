b6f913da644a0e1447f41a10b1036422816df1e6
ian-mcconnell <ianmcconnell376@gmail.com>
2019-11-13 13:57:15 -0500

    Added menu music and volume controller
    volume can be adjusted using sliders and the setting sticks between scenes

--- Tank Toss/Assets/Scripts/CameraFollow.cs
+++ Tank Toss/Assets/Scripts/CameraFollow.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform playerTank;
+    public float HorizontalOffset = 0f;
+    public float VerticalOffset = 7f;
 
-    void FixedUpdate ()
+    private void FixedUpdate ()
     {
-        transform.position = new Vector3(playerTank.position.x, playerTank.position.y, transform.position.z);
+        transform.position = new Vector3(playerTank.position.x, playerTank.position.y, transform.position.z) + new Vector3(-HorizontalOffset, VerticalOffset, 0f);
     }
 }
---
+++ Tank Toss/Assets/Scripts/Game Scripts/SoundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+
+    private void OnEnable()
+    {
+        if (!PlayerPrefs.HasKey("volume"))
+        {
+            PlayerPrefs.SetFloat("volume", 1.0f);
+        }
+
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    private void Update()
+    {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.Save();
+    }
+}
