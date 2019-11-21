cf5939616c2c82eaabc8a112fe0e6457291310f0
NoireSync <shantellburrowes@yahoo.com>
2019-11-20 19:07:10 -0500

    Splash n how to

---
+++ Tank Toss/Assets/Scripts/Menu Scripts/SplashFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashFade : MonoBehaviour
+{
+    public Image splashPic;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashPic.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    void FadeIn()
+    {
+        splashPic.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        splashPic.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
+
+
